8c0dc1621e317f773cdef715a67cf03e784d0760
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2023-04-19 23:34:55 +0530

    url

--- donation_post/urls.py
+++ donation_post/urls.py
@@ -4,6 +4,6 @@ from rest_framework import routers
 
 urlpatterns = [
     path('list/', views.DonationPostListView.as_view()),
-    path('post/', views.DonationPostView.as_view()),
-    path('post/new', views.DonationPostCreateView.as_view())
+    path('all/', views.DonationPostView.as_view()),
+    path('new/', views.DonationPostCreateView.as_view())
 ]
